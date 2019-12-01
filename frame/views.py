44bc37d40ae1d7afb642da0f33b4d2d27f5b0b50
Vladislav <vladeslaw@gmail.com>
2019-12-01 10:57:41 +0300

    В приложении views.py добавлена табличка 2х2 В глобальных настройках добавлен хост 127.0.0.1

--- frame/views.py
+++ frame/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+'\'строка\'' ' \' - экран '
 
 def index(request):
-    return HttpResponse('Frame is a part of the window')
+    return HttpResponse(
+        '''<table border="1">
+        <tr>
+            <td width="100" height="50" colspain="2">Ячейка - 1
+            </td>
+            <td width="100" height="50" colspain="2"> Ячейка - 2
+            </td>
+        </tr>
+        <tr>
+            <td width="100" height="50"> Ячейка - 3
+            </td>
+            <td width="100" height="50"> Ячейка - 4
+            </td>
+        </tr>
+    </table>
+        ''')
+
 # Create your views here.
