76c1231b2d62e0736f494e91603606636870b9b0
Vladislav <vladeslaw@gmail.com>
2019-12-01 12:52:04 +0300

    Сгенерирована/обавлена таблица Пифагора

--- frame/views.py
+++ frame/views.py
@@ -1,23 +1,47 @@
 from django.shortcuts import render
+
+# my stuff here
 from django.http import HttpResponse
+from django.template import loader
+
 '\'строка\'' ' \' - экран '
 
 def index(request):
-    return HttpResponse(
-        '''<table border="1">
-        <tr>
-            <td width="100" height="50" colspain="2">Ячейка - 1
-            </td>
-            <td width="100" height="50" colspain="2"> Ячейка - 2
-            </td>
-        </tr>
-        <tr>
-            <td width="100" height="50"> Ячейка - 3
-            </td>
-            <td width="100" height="50"> Ячейка - 4
-            </td>
-        </tr>
-    </table>
-        ''')
+    template = loader.get_template('resolution_frames/index.html')
+    context = {
+        'window_height': 50,
+        'window_width': 100,
+        'raws': [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        'values1': values[1],
+        'values2': values[2],
+        'values3': values[3],
+        'values4': values[4],
+        'values5': values[5],
+        'values6': values[6],
+        'values7': values[7],
+        'values8': values[8],
+        'values9': values[9],
+    }
+    return HttpResponse(template.render(context))
+
+def making_numbers():
+    raws_and_cols = 9
+    all_numbers = []
+    for i in range(raws_and_cols):
+        i += 1
+        all_numbers.append(i)
+    return all_numbers
+
+def making_table():
+    all_numbers = making_numbers()
+    table = {}
+    for i in all_numbers:
+        table[i] = []
+        table[i].append(i)
+        for j in all_numbers:
+            table[i].append(i*j)
+    print(table)
+    return table
+values = making_table()
 
 # Create your views here.
