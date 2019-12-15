4954281cfd2da80fdeddb400c21dd0e8be042200
Vladislav <vladeslaw@gmail.com>
2019-12-15 17:21:33 +0300

    Изменён settings для collectstatic, теперь есть доступ к файлам и можно сделать статик

--- frame/models.py
+++ frame/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import int_list_validator
+from django.core.validators import RegexValidator
 
 
 # Create your models here.
@@ -12,3 +13,9 @@ class Frame(models.Model):
     # будет создан автоматически
     window_height = models.IntegerField(default=50)
     # просто создаём "табличку" в базе данных с полем названия переменной
+    window_width = models.IntegerField(default=50)
+    scheme = models.CharField(
+        validators=[
+            RegexValidator('\{(\d+:\[\d+(,\d+)*\],)*\}')],
+        default='{}',
+        max_length=256)
