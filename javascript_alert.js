cb77c322ce2c5d92b52a6e02d4c7866647dad256
Vladislav <vladeslaw@gmail.com>
2019-12-18 16:26:33 +0300

    Была изменена директория static_root. basedirectory не лучшее место для хранения static files

--- javascript_alert.js
+++
@@ -1,3 +0,0 @@
-function alert_java() {
-    alert('Hey, remember to tell your friends about');
-}
--- page_alert.js
+++
@@ -1,5 +0,0 @@
-function cry() {
-    alert('Число изменено.');
-}
-
-/*http://127.0.0.1:8000/static/page_alert.js */
