845be6c308c7fdbf511a468592e6c80f1775ef93
Vladislav <vladeslaw@gmail.com>
2019-12-22 16:39:58 +0300

    Сделан тест для приложения frame

--- frame/tests.py
+++ frame/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
+from .models import Frame
+
+
 # Create your tests here.
+class FrameTest(TestCase):
+    def setUp(self):
+        # self.client = client
+        Frame(
+            window_width=10,
+            window_height=10,
+        ).save()
